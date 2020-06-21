92fa98d6575e4c4a9466b6071c6a9cd240051e30
yogiraj <bhoomkar99@gmail.com>
2020-06-21 22:08:20 +0530

    Done with the Product Category and Product List Display

---
+++ lib/models/ProductCategoryClass.dart
@@ -0,0 +1,15 @@
+import 'package:fthvendor/models/productCard.dart';
+
+class ProductCategoryClass{
+  String categoryName;
+  int categoryId;
+
+   ProductCategoryClass(categoryName,categoryId){
+    this.categoryName=categoryName;
+    this.categoryId=categoryId;
+  }
+  String getter(){
+    return categoryName;
+  }
+}
+
